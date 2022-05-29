68264692fca09cc8a4ebfc116b434e253f3f0c7d
kadev2176 <kevinchristopher2176@gmail.com>
2022-05-29 05:10:30 +0100

    add more fields to Transfer

--- src/handlers/transfer.ts
+++ src/handlers/transfer.ts
@@ -1,6 +1,7 @@
 import { SubstrateEvent } from "@subql/types";
 import { Transfer } from "../types";
 import { ensureAccount } from "./account";
+import { ensureBlock } from "./block";
 import { addTransferToDay } from "./day";
 
 export async function createTransfer(event: SubstrateEvent): Promise<void> {
@@ -8,11 +9,16 @@ export async function createTransfer(event: SubstrateEvent): Promise<void> {
   const from = await ensureAccount(fromId);
   const to = await ensureAccount(toId);
   const value = BigInt(balance);
+  const block = await ensureBlock(event.block.block.header.number.toString())
   const entity = Transfer.create({
-    id: `${event.block.block.header.number}-${event.idx}`,
+    id: `${block.id}-${event.idx}`,
     fromId: from.id,
     toId: to.id,
-    value: value
+    value: value,
+    blockId: block.id,
+    blockNumber: block.number,
+    eventIndex: event.idx,
+    extrinsicIndex: event.extrinsic?.idx
   });
   await entity.save();
   await addTransferToDay(event.block.timestamp, value);
